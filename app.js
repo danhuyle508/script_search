5be968e4c1d38342cc62fa6d563e7e9c420d9dd2
Dan Le <Danhuyle508@gmail.com>
2019-07-29 12:41:17 -0700

    started

---
+++ app.js
@@ -0,0 +1,19 @@
+
+( function () {
+    'use strict';
+    // Keep track of the output commands
+    const outputCommands = [];
+  
+    // Add button to page
+    const button = document.createElement( 'button' );
+    button.textContent = 'Script Search';
+    button.className = 'btn btn-sm';
+    document.body
+      .getElementsByClassName( '.neptune-grid' )[ 0 ]
+      .appendChild( button );
+    
+    button.onclick = function (){
+        let body = document.body.getElementsByClassName('jobs-search-two-pane_details')
+        body.search()
+    }  
+}) 
